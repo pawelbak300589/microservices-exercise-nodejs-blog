5a319ec8e7928c3a5c094a999d46e1ee1e31f591
Pawel Bak <pawelbak300589@gmail.com>
2020-07-25 14:45:08 +0100

    Add client folder

---
+++ client/src/CommentList.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const CommentList = ({ postId }) => {
+    const [comments, setComments] = useState([]);
+
+    const fetchComments = async () => {
+        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
+
+        setComments(res.data);
+    };
+
+    useEffect(() => {
+        fetchComments();
+    }, []);
+
+    const renderComments = comments.map(comment => {
+        return <li key={comment.id}>{comment.content}</li>;
+    });
+
+    return (
+        <ul>
+            {renderComments}
+        </ul>
+    );
+};
+
+export default CommentList;
