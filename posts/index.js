1059b6b339266b8ce850b6e4f97c937fa01286db
Pawel Bak <pawelbak300589@gmail.com>
2020-10-05 11:44:49 +0100

    Configuring microservices app with kubernates

--- posts/index.js
+++ posts/index.js
@@ -13,7 +13,7 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
@@ -21,7 +21,7 @@ app.post('/posts', async (req, res) => {
         id, title
     };
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: {
             id, title
@@ -38,5 +38,6 @@ app.post('/events', async (req, res) => {
 });
 
 app.listen(4000, () => {
+    console.log('v2');
     console.log('Listening on 4000');
 });
